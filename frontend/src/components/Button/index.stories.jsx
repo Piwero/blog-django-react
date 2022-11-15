8d13acff7db7fecf46bad1f0fff4b299f58ef468
Nacho Pinero <piwero@gmail.com>
2022-11-15 21:07:47 +0000

    Rename button files

--- frontend/src/components/Button/index.stories.jsx
+++ frontend/src/components/Button/index.stories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button } from './Button.jsx';
+import { Button } from './index.jsx';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
