1d83ac37b387b36cdeefa6820f095ac3abe33c43
Nacho Pinero <piwero@gmail.com>
2022-11-15 08:45:56 +0000

    Create setting Icon

---
+++ frontend/src/Icons/SettingIcon/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Cog6ToothIcon } from '@heroicons/react/24/solid'
+
+const SettingIcon =()=>(
+    < Cog6ToothIcon className="h-6 w-6" />
+)
+
+export default SettingIcon;
